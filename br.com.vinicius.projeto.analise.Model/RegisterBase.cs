040f697f5f5bfd6bacd9711d7e49e72f6f93a077
Cliente <Cliente@CLIENTE-PC>
2017-11-13 19:23:14 -0200

    inserindo provider/connectionstring do mysql

--- br.com.vinicius.projeto.analise.Model/RegisterBase.cs
+++ br.com.vinicius.projeto.analise.Model/RegisterBase.cs
@@ -9,11 +9,13 @@ namespace br.com.vinicius.projeto.analise.Model
 {
     public abstract class RegisterBase
     {
-        protected string connectionString = @"data source=VINICIUS-PC\SQLEXPRESS;Initial Catalog=Analise; Integrated Security = SSPI";
+        protected string connectionString = "server=127.0.0.1;uid=root;pwd=admin;database=analise";
+       // protected string connectionString = @"data source=VINICIUS-PC\SQLEXPRESS;Initial Catalog=Analise; Integrated Security = SSPI";
         //"Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)" +
         //"(HOST=MYHOST)(PORT=1527))(CONNECT_DATA=(SID=MYSERVICE)));" +
         //"User Id=MYUSER;Password=MYPASS;";
-        public  string provider = @"System.Data.SqlClient";
+        //public  string provider = @"System.Data.SqlClient"; // sqlserver
+        public  string provider = @"MySql.Data.MySqlClient";  //mysql
         public DbConnection connection = null;
         public DbProviderFactory factory;
 
