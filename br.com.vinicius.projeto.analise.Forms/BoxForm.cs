e82729aaa23406ea771f914d01220774fd1e0b50
vinicius <vinicius@VINICIUS-PC>
2017-11-19 22:03:16 -0200

    m

---
+++ br.com.vinicius.projeto.analise.Forms/BoxForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace br.com.vinicius.projeto.analise.Forms
+{
+    public partial class BoxForm : Form
+    {
+        public BoxForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
